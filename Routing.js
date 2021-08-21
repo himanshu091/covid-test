e5a7adb3edc2a0d3d8f42835c9274d39c2b2824a
Chouhans <jaymanyoo3@gmail.com>
2021-08-21 17:50:37 +0530

    added routes and reducers

---
+++ Routing.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Home from './screens/Home';
+import Login from './screens/Login';
+import Register from './screens/Register';
+
+
+
+// const Tab = createBottomTabNavigator();
+// function HomeTabs() {
+//     return (
+//       <Tab.Navigator>
+//         <Tab.Screen name="HomeScreen" component={Home} />
+//         <Tab.Screen name="Search" component={Search} />
+//         <Tab.Screen name="MyReservations" component={MyReservations} />
+//         <Tab.Screen name="Profile" component={Profile} />
+//       </Tab.Navigator>
+//     );
+//   }
+  
+const Stack = createStackNavigator();
+function Routing() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator headerMode="none" screenOptions={{animationEnabled: false}}>
+                <Stack.Screen name="Home" component={Home}/>
+                
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Register" component={Register} />
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default Routing;
