381b5309c4dc18570af0c9b144300acd7db737ac
Chouhans <jaymanyoo3@gmail.com>
2021-08-22 12:37:57 +0530

    Completed Users static screen

--- components/UserAccordian.js
+++ components/UserAccordian.js
@@ -4,6 +4,7 @@ import upcheveron from '../assets/ch_up.png'
 import downCheveron from '../assets/chdown.png'
 import delete_icon from '../assets/delete.png'
 import eye_icon from '../assets/eye.png'
+import edit_icon from '../assets/edit.png'
 const UserAccordian = () => {
     const [open, setOpen] = useState(false)
     return (
@@ -25,26 +26,34 @@ const UserAccordian = () => {
             </View>
             {open && <View style={styles.insideData}>
                 <View style={styles.dflex}>
-                    <Text style={styles.key}>ID :</Text>
+                    <Text style={styles.key}>ID        :    </Text>
                     <Text style={styles.value}>APT0013</Text>
                 </View>
                 <View style={styles.dflex}>
-                    <Text style={styles.key}>Email :</Text>
+                    <Text style={styles.key}>Email  :    </Text>
                     <Text style={styles.value}>weandd@gmail.com</Text>
                 </View>
                 <View style={styles.dflex}>
-                    <Text style={styles.key}>Date :</Text>
+                    <Text style={styles.key}>Date    :    </Text>
                     <Text style={styles.value}>22 Feb 2021</Text>
                 </View>
                 <View style={styles.dflex}>
-                    <Text style={styles.key}>Time :</Text>
+                    <Text style={styles.key}>Time   :    </Text>
                     <Text style={styles.value}>09:50 PM</Text>
                 </View>
                 <View style={styles.dflex}>
-                    <Text style={styles.key}>Result :</Text>
-                    <Text style={styles.value}>Negative</Text>
+                    <Text style={styles.key}>Result :    </Text>
+                    <Text style={[styles.value, {color:'#6da06d'}]}>Negative</Text>
+                    <Text style={[styles.value, {color:'#de273a'}]}>Positive</Text>
                 </View>
-                
+                <View style={[styles.dflex, styles.status]}>
+                    <Text style={styles.key}>Status:</Text>
+                    <Text style={[styles.value, {color:'#6da06d'}]}>Approved</Text>
+                </View>
+                <TouchableOpacity style={styles.editBtn}>
+                    <Image source={edit_icon} style={{height:15, width:15}}/>
+                    <Text style={styles.editTxt}>Edit</Text>
+                </TouchableOpacity>
             </View>}
         </View>
     )
@@ -62,9 +71,7 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems:'center',
 
-        paddingBottom: 15,
-        borderBottomColor: '#e6e6e6',
-        borderBottomWidth: 2
+        
     },
     green_txt:{
         color: '#499b4a',
@@ -76,7 +83,7 @@ const styles = StyleSheet.create({
     },
     normalTxt:{
         fontWeight: '700',
-        color: '#474747'
+        color: '#4f4f4f'
     },
     openIcon:{
         backgroundColor: '#e3f0f6',
@@ -92,6 +99,36 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         paddingTop: 15,
         paddingHorizontal: 10,
-        
+        paddingBottom: 15,
+        borderBottomColor: '#e6e6e6',
+        borderBottomWidth: 2
+    },
+    insideData:{
+        marginTop: 7
+    },
+    key:{
+        color: '#b7b7b7',
+        fontWeight: '700'
+    },
+    value:{
+        color: '#4e4e4c',
+        fontWeight: '700'
+    },
+    status:{
+        position: 'absolute',
+        bottom: 0,
+        right: 0
+    },
+    editBtn:{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    editTxt:{
+        color: '#518ac0',
+        fontWeight: '700'
     }
 })
--- screens/Home.js
+++ screens/Home.js
@@ -1,23 +1,152 @@
-import React from 'react'
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native'
 import BottomTab from '../components/BottomTab'
 import UserAccordian from '../components/UserAccordian'
-
+import search_icon_blue from '../assets/search_icon_blue.png'
 const Home = () => {
+    const [query, setQuery] = useState("")
+    const handleSubmit = () => {
+
+    }
     return (
-        <SafeAreaView style={{flex: 1, backgroundColor: '#fafafa'}}>
-            <ScrollView>
-            <Text>Home Page</Text>
-            <UserAccordian/>
-            <UserAccordian/>
-            <UserAccordian/>
-            <UserAccordian/>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#fafafa', paddingBottom: 70 }}>
+            <View style={{ paddingHorizontal: 10 }}>
+                <View style={styles.header}>
+                    <View style={styles.headerPart1}>
+                        <Text style={styles.headerTxt1}>Users</Text>
+                        <Text style={styles.headerTxt2}>(Total 50+)</Text>
+                    </View>
+                    <TouchableOpacity style={styles.headerBtn}>
+                        <Text style={styles.btnTxt}>+ ADD NEW USER</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.searchBar}>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={setQuery}
+                        value={query}
+                        placeholder="Search the place..."
+
+                        onSubmitEditing={() => { handleSubmit(); }}
+                        blurOnSubmit={false}
+                    />
+                    <TouchableOpacity style={styles.searchBtn} onPress={() => { handleSubmit(); }}>
+                        <Image source={search_icon_blue} style={{ width: 20, height: 20 }} />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.thead}>
+                    <TouchableOpacity>
+                        <Text style={styles.th}>ID</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={styles.th}>Name</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={styles.th}>Actions</Text>
+                    </TouchableOpacity>
+                    <View>
+                        <Text style={styles.th}></Text>
+                    </View>
+                </View>
+            </View>
+            
+            <ScrollView style={{ paddingHorizontal: 10 }}>
+
+
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
+                <UserAccordian />
             </ScrollView>
-            <BottomTab/>
+            <BottomTab />
         </SafeAreaView>
     )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 20
+    },
+    headerPart1: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    headerTxt1: {
+        color: '#2e2e2c',
+        fontSize: 18,
+        fontWeight: '700',
+        marginRight: 7
+    },
+    headerTxt2: {
+        color: '#b4b4b4',
+        fontSize: 12,
+        fontWeight: '700'
+    },
+    headerBtn: {
+        borderRadius: 25,
+        backgroundColor: '#fff',
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+
+        elevation: 11
+    },
+    btnTxt: {
+        color: '#3585a9',
+        fontWeight: '700',
+        fontSize: 15,
+    },
+    searchBtn: {
+        backgroundColor: '#edf8ff',
+        borderRadius: 70,
+        paddingHorizontal: 12,
+        paddingVertical: 12,
+        margin: 5
+    },
+    searchBar: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+
+        paddingLeft: 5,
+        backgroundColor: '#fff',
+        borderWidth: 2,
+        borderColor: '#f3f3f3',
+        borderRadius: 8,
+        marginBottom: 20
+    },
+    input: {
+        width: '75%',
+    },
+    thead:{
+        backgroundColor: '#003686',
+        borderRadius: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    th:{
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 15
+    }
+})
